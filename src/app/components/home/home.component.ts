0ded307da8d80a0db96f095f9f24a386b1502c1d
Huguez <chuguez@quantumbit.mx>
2019-10-09 17:44:35 -0700

    se agrego navbar

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class HomeComponent implements OnInit {
+  
 
-  constructor() { }
+  constructor( ) { }
 
   ngOnInit() {
   }

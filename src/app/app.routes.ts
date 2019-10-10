3be58aa749a4c947617f1b70db3ddbc8940b58be
Carlos Huguez <carlos.huguez@gmail.com>
2019-10-09 22:48:58 -0700

    se arreglo el problema del navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,5 @@ import { SearchComponent } from './components/search/search.component';
 export const ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'search', component: SearchComponent },
-    { path: '', pathMatch: 'full', redirectTo: 'home' },
-    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+    { path: '', pathMatch: 'full', redirectTo: 'home' }
 ];
